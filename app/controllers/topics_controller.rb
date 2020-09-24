63645fc1bf98e99786722d6f4869022146e00895
Gicarr1998 <71775235+Gicarr1998@users.noreply.github.com>
2020-09-24 22:04:38 +0800

    This is my first commit

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,48 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def new
+    @topic = Topic.new
+  end
+  
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+      if @topic.update(topic_params)
+        redirect_to root_url
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    if @topic.destroy
+      redirect_to root_url
+    else
+      redirect_to root_url
+    end
+  end
+end
+private
+def topic_params
+  params.require(:topic).permit(:title, :description)
+end
---
+++ app/models/topic.rb
@@ -0,0 +1,4 @@
+class Topic < ApplicationRecord
+  validates :title, presence: true, length: {minimum:5}
+  validates :description, presence: true, length: {minimum:7}
+end
